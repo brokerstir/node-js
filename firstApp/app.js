0079b4a0126cddc06144da7556521a1174b3ce51
Robert Ris <take1risk@gmail.com>
2019-11-24 20:14:46 -0600

    use http module

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,9 +1,17 @@
-const Logger  = require('./logger');
-const logger = new Logger();
+const http = require('http');
 
-// Register a listener
-logger.on('messageLogged', (arg) => {
-  console.log('Listener called', arg);
+const server =  http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello, World!');
+    res.end();
+  }
+
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();  
+  }
 });
 
-logger.log('Hello, World!');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
