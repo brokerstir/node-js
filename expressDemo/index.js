cd30840977879f9ae940da14a79675953bd3051a
Robert Ris <take1risk@gmail.com>
2019-12-03 20:18:21 -0600

    handle put and delete req

--- expressDemo/index.js
+++ expressDemo/index.js
@@ -19,18 +19,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
+  const { error } = validateCourse(req.body);
 
-  const result = Joi.validate(req.body, schema);
-  //console.log(result);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-
+  if (error) return  res.status(400).send(error.details[0].message);
+    
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -39,11 +31,40 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
-app.get('/api/courses/:id', (req, res) => {
+app.put('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('Course not found.');
+  if (!course) return res.status(404).send('Course not found.');
+
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(result.error.details[0].message);
+  
+  course.name = req.body.name;
   res.send(course);
 });
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course not found.');
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course not found.');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('port:', port));
